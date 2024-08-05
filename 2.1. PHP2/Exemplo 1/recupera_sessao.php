9ba2b3a2f47c933c0a59914638cde9b130f90e05
JUSÉ <jose.p2005@aluno.ifsc.edu.br>
2024-08-05 09:07:17 -0300

    PHP 2
    Cookies e Sessões

---
+++ 2.1. PHP2/Exemplo 1/recupera_sessao.php
@@ -0,0 +1,4 @@
+<?php 
+    session_start();
+    echo "<h2> Na sessão existe o txt </h2>". $_SESSION['txt'];
+?>
---
+++ 2.1. PHP2/Exemplo 2/login.php
@@ -0,0 +1,7 @@
+<form action="autenticacao.php" method="post">
+    <label for="login">Login</label>
+    <input type="text" id="login" name="login"> <br>
+    <label for="passwd">Senha</label>
+    <input type="password" id="passwd" name="passwd">
+    <input type="submit" value="Autenticar">
+</form>
